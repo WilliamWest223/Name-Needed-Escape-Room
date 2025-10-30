56d68d6a36ad8bb660475f1e1515f8935d4ca6d5
shaktikaver2001 <shaktikaver@gmail.com>
2025-10-30 10:09:17 -0400

    test add

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.escaperoom {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
-
+    requires junit;
     opens com.escaperoom to javafx.fxml;
     exports com.escaperoom;
     exports com.escapenexus;
